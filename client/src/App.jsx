af86a5ccd141df5a46bce52faacbd08981e6b9f1
Santhosh-2304 <lionelsanthosh2004@gmail.com>
2025-10-06 19:52:41 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
+// client/src/App.jsx
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Lights from './pages/Lights';
+import User from './pages/User';
+import UserCreate from './pages/UserCreate';
+
+
 
 export default function App() {
   return (
@@ -11,8 +16,8 @@ export default function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/homes/:id/lights" element={<Lights />} />
-      {/* fallback */}
-      <Route path="*" element={<Navigate to="/login" />} />
+      <Route path="/User" element={<User />}/>
+      <Route path="/UserCreate" element={<UserCreate/>}/>
     </Routes>
   );
 }
