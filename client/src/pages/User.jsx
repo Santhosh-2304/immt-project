61de9f0dbfda4d5a0264b457568d242e83229c80
Santhosh-2304 <lionelsanthosh2004@gmail.com>
2025-10-11 12:59:04 +0530

    Update User.jsx

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import api from '../api';
 
 const User = () => {
@@ -8,7 +8,7 @@ const User = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await api.get("/userRoutes");
+        const response = await api.get("/users");
         setUsers(response.data);
       } catch (error) {
         console.log("Error while fetching data", error);
@@ -19,10 +19,9 @@ const User = () => {
 
   const deleteUser = async (userId) => {
     await api
-      .delete(`/userRoutes/delete/${userId}`)
+      .delete(`/users/delete/${userId}`)
       .then((response) => {
         setUsers((prevUser) => prevUser.filter((user) => user._id !== userId));
-        toast.success(response.data.message, { position: "top-right" });
       })
       .catch((error) => {
         console.log(error);
@@ -34,7 +33,8 @@ const User = () => {
     <div className='userTable'>
         <div className='userHeader'> <button className="btn outline" onClick={() => navigate('/dashboard')} style={{height:"40%"}}>Back</button><h1>Users</h1>
         <button className='user-add' onClick={() => navigate('/UserCreate')}><i class="fa-solid fa-user-plus"></i> Create User</button></div> 
-        <table className='table' style={{borderCollapse:"collapse"}}>
+        <div className="userTable-content">
+        <table className='table slide-up' style={{borderCollapse:"collapse"}}>
             <thead>
                 <tr>
                     <th scope="col">S.No</th>
@@ -54,8 +54,8 @@ const User = () => {
                 <td>{user.email} </td>
                 <td>{user.type}</td>
                 <td>{user.isActive ? '✅' : '❌'}</td>
-                <td><button type="button" class="btn btn-warning" style={{backgroundColor:"orange", marginRight:"5px"}}><i class="fa-solid fa-pen-to-square"></i></button>
-                <button type="button" class="btn btn-danger" style={{backgroundColor:"red"}}><i class="fa-solid fa-trash"></i> </button>
+                <td><Link to ={`/update/`+user._id} type="button" class="btn btn-warning" style={{backgroundColor:"#81a7ffff", marginRight:"5px",paddingTop:"5px"}}><i class="fa-solid fa-pen-to-square"></i></Link>
+                <button type="button" onClick={() => { if (window.confirm("Are you sure you want to delete this user?")) {deleteUser(user._id);}}}class="btn btn-danger" style={{backgroundColor:"#ff5050ff"}}><i class="fa-solid fa-trash"></i> </button>
                                    
                 </td>
               </tr>
@@ -63,6 +63,7 @@ const User = () => {
           })}
         </tbody>
         </table>
+    </div>
     </div>
   )
 }
